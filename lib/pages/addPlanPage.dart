b5ebb2e753eb4bc28200e1603ea13d9b64e1e716
404CoderM <117585798+404CoderM@users.noreply.github.com>
2023-08-07 23:44:08 +0530

    goals page connected to database

--- lib/pages/addPlanPage.dart
+++ lib/pages/addPlanPage.dart
@@ -1,4 +1,6 @@
 import 'package:budgettrack/components/button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 //import 'package:flutter/material.dart';
 import '../components/datePicker.dart';
@@ -16,6 +18,21 @@ class _AddPlanState extends State<AddPlan> {
   final planAmountController = TextEditingController();
   DateTime? selectedStartDate;
   DateTime? selectedEndDate;
+
+  String? selectedCategory;
+  List<String> expenceCategories = [
+    'Food',
+    'Shopping',
+    'Travel',
+    'Bills',
+    'Entertainment',
+    'Health',
+    'Education',
+    'Gifts',
+    'Donations',
+    'Rental',
+    'Others',
+  ];
 
   //Function to show the date picker for start date
   Future<void> _selectStartDate(BuildContext context) async {
@@ -57,6 +74,91 @@ class _AddPlanState extends State<AddPlan> {
     }
   }
 
+  //Validation
+  _validateFields() {
+    if (planAmountController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+            'Please Enter The Amount',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          backgroundColor: Colors.red[300],
+        ),
+      );
+    } else if (selectedCategory == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+            'Please Select a Category',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          backgroundColor: Colors.red[300],
+        ),
+      );
+    } else if (selectedStartDate == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+            'Please Select a Start Date',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          backgroundColor: Colors.red[300],
+        ),
+      );
+    } else if (selectedEndDate == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text(
+            'Please Select an End Date',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          backgroundColor: Colors.red[300],
+        ),
+      );
+    } else {
+      addGoalsToFirestore(
+        //FirebaseAuth.instance.currentUser!.uid,
+        selectedCategory!,
+        planAmountController.text as int,
+        selectedStartDate.toString(),
+        selectedEndDate.toString(),
+      );
+      Navigator.pop(context);
+    }
+  }
+
+  //Method add goals to the database
+  Future<void> addGoalsToFirestore(
+    //String userId,
+    String selectedCategory,
+    int planAmountController,
+    String selectedStartDate,
+    String selectedEndDate,
+  ) async {
+    try {
+      final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      await firestore.collection('goalsID').add({
+        //'userId': userId,
+        'category': selectedCategory,
+        'amount': planAmountController.toString(),
+        'startDate': selectedStartDate.toString(),
+        'endDate': selectedEndDate,
+      });
+    } catch (e) {
+      print('Goals adding to firestore failed');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,25 +188,12 @@ class _AddPlanState extends State<AddPlan> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 20),
-
-              // Add plan text
-
-              const Text(
-                'Add Plan',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
               // paln title
 
-              const SizedBox(height: 25),
+              const SizedBox(height: 40),
 
               const Text(
-                'Title',
+                'Expence Category',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 20,
@@ -112,15 +201,41 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 15),
 
               // plan text field
-              PlanTextField(
-                controller: planTitleController,
-                hintText: 'Enter plan title',
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.grey)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: DropdownButton<String>(
+                      underline: const SizedBox(),
+                      icon: const Padding(
+                        padding: EdgeInsets.only(left: 205),
+                        child: Icon(Icons.arrow_downward_sharp),
+                      ),
+                      hint: const Text('Select category'),
+                      value: selectedCategory,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCategory = value;
+                        });
+                      },
+                      items: expenceCategories
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          child: Text(value),
+                          value: value,
+                        );
+                      }).toList()),
+                ),
               ),
 
-              const SizedBox(height: 20),
+              const SizedBox(height: 25),
 
               // plan amount title
               const Text(
@@ -132,7 +247,7 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 15),
 
               // plan anount text field
               PlanTextField(
@@ -140,7 +255,7 @@ class _AddPlanState extends State<AddPlan> {
                 hintText: 'Enter plan amount',
               ),
 
-              const SizedBox(height: 20),
+              const SizedBox(height: 25),
 
               // start date title
               const Text(
@@ -152,7 +267,7 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 15),
 
               // start date text field
               InkWell(
@@ -163,7 +278,7 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 20),
+              const SizedBox(height: 25),
 
               // end date title
 
@@ -176,7 +291,7 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 15),
 
               // end date text field
 
@@ -188,12 +303,12 @@ class _AddPlanState extends State<AddPlan> {
                 ),
               ),
 
-              const SizedBox(height: 25),
+              const SizedBox(height: 30),
 
               //add plan button
 
               MyButton(
-                onTap: () => {},
+                onTap: () => _validateFields(),
                 text: 'Add Plan',
               ),
             ],
